18f530c125c506e0082328863dbc7e43b11a297e
Baotran201806 <1851050153tran@ou.edu.vn>
2022-10-23 19:36:01 +0700

    final

---
+++ src/utilities/Settings/config.js
@@ -0,0 +1,18 @@
+//Định nghĩa tham số cố định
+
+export const DOMAIN = "https://elearningnew.cybersoft.edu.vn";
+export const TOKEN = "accessToken";
+export const GROUPID = "GP01";
+export const TokenCyberSoft = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJGcm9udCBFbmQgNzIiLCJIZXRIYW5TdHJpbmciOiIxNC8wMi8yMDIzIiwiSGV0SGFuVGltZSI6IjE2NzYzMzI4MDAwMDAiLCJuYmYiOjE2NTAzODc2MDAsImV4cCI6MTY3NjQ4MDQwMH0.e3UrKdKqwFislz0cqribEEthuaW4HOuD4xwr1CTRQwg";
+export const TYPE_USER = 'TYPE_USER'; 
+export const USER_SIGNIN = "USER_SIGNIN";
+
+export const CATE_BACKEND = "BackEnd";
+export const CATE_DESIGN = "Design";
+export const CATE_DIDONG = "DiDong";
+export const CATE_FRONTEND = "FrontEnd";
+export const CATE_FULLSTACK = "FullStack";
+export const CATE_TUDUY = "TuDuy";
+
+
+
