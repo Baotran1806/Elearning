979c6d77eced09b9f826570244e17bb48c49997e
Baotran201806 <1851050153tran@ou.edu.vn>
2023-04-04 15:14:38 +0700

    token

--- src/utilities/Settings/config.js
+++ src/utilities/Settings/config.js
@@ -3,7 +3,7 @@
 export const DOMAIN = "https://elearningnew.cybersoft.edu.vn";
 export const TOKEN = "accessToken";
 export const GROUPID = "GP01";
-export const TokenCyberSoft = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJGcm9udCBFbmQgNzIiLCJIZXRIYW5TdHJpbmciOiIxNC8wMi8yMDIzIiwiSGV0SGFuVGltZSI6IjE2NzYzMzI4MDAwMDAiLCJuYmYiOjE2NTAzODc2MDAsImV4cCI6MTY3NjQ4MDQwMH0.e3UrKdKqwFislz0cqribEEthuaW4HOuD4xwr1CTRQwg";
+export const TokenCyberSoft = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJUcmFpbmluZyBnaeG6o25nIHZpw6puIGN5YmVyc29mdCAyMDIyIiwiSGV0SGFuU3RyaW5nIjoiMzAvMTEvMjAyOCIsIkhldEhhblRpbWUiOiIxODU5MTU1MjAwMDAwIiwibmJmIjoxNTg0MjkxNjAwLCJleHAiOjE4NTkzMDI4MDB9.9nOWAOoO7NtipuO-A-4_8kwzVp7j5HSdXjEegqTgcXI";
 export const TYPE_USER = 'TYPE_USER'; 
 export const USER_SIGNIN = "USER_SIGNIN";
 
